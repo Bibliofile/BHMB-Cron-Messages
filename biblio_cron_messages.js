7d06975202d012673e7deb564a09f54dd253ad01
Bibliofile <bibliofilegit@gmail.com>
2016-10-09 20:05:14 -0600

    6.0 API support

--- biblio_cron_messages.js
+++ biblio_cron_messages.js
@@ -1,81 +1,102 @@
-/*jshint 
-    esnext:        true,
-    browser:    true,
-    devel:        true,
-    unused:        true,
-    undef:        true
+/*jshint
+    esnext: true,
+    browser: true,
+    devel: true,
+    unused: true,
+    undef: true
 */
 /*global
     MessageBotExtension
 */
 
 var biblio_cron_messages = MessageBotExtension('biblio_cron_messages');
-biblio_cron_messages.setAutoLaunch(true);
 
-biblio_cron_messages.uMID = 0;
-biblio_cron_messages.messages = localStorage.getItem('biblio_cron_messages_messages' + window.worldId);
-biblio_cron_messages.messages = (biblio_cron_messages.messages === null) ? [] : JSON.parse(biblio_cron_messages.messages);
-biblio_cron_messages.uninstall = function() {
-    Object.keys(localStorage).forEach(function(key) {
-        if (key.indexOf('biblio_cron_messages_messages') === 0) {
-            localStorage.removeItem(key);
+(function(ex, storage, ui) {
+    ex.setAutoLaunch(true);
+    ex.running = true;
+    ex.uninstall = function() {
+        //Remove stored messages
+        storage.removeNamespace('biblio_cron_messages');
+        //Kill the listener
+        ex.running = false;
+    };
+
+    ex.tab = ui.addTab('Cron Messages', 'messages');
+    ex.tab.innerHTML = '<style>#biblio_cron_messages_h { margin: 0 0 5px 0;} #cMsgs{padding-top: 8px;margin-top: 8px;border-top: 1px solid;height: calc(100vh - 185px);}</style><template id="biblio_cron_messages_template"><div class="third-box"><label>Minute Value</label><input value="0" pattern="[0-9\, ]{1,}" placeholder="0-59"><br><label>Message: </label><input class="m"><br><a>Delete</a></div></template><h3 id="biblio_cron_messages_h">These are sent on a regular schedule.</h3><span class="descdet">If minute value is set to 0, the message will be sent at 1:00, 2:00... if it is set to 0,30, it will be sent at 1:00, 1:30, 2:00....</span><span class="top-right-button">+</span><div id="cMsgs"></div>';
+
+    //IE Template fix
+    (function(template) {
+        var content = template.childNodes;
+        var fragment = document.createDocumentFragment();
+
+        for (var i = 0; i < content.length; i++) {
+            fragment.appendChild(content[i]);
         }
+
+        template.content = fragment;
+    }(ex.tab.querySelector('template')));
+
+    function addMessage(values) {
+        ui.buildContentFromTemplate('#biblio_cron_messages_template', '#cMsgs',
+        [
+            {selector: 'input', value: values.minutes || 0},
+            {selector: '.m', value: values.message || ''},
+        ]);
+
+        ex.tab.querySelector('#cMsgs > div:last-of-type a').addEventListener('click', deleteMessage);
+
+        //No need to save here.
+    }
+    ex.tab.querySelector('.top-right-button').addEventListener('click', function() {
+        addMessage({});
     });
-};
 
-biblio_cron_messages.addMainTab('cronTab', 'Cron Messages');
-biblio_cron_messages.mainTabs.cronTab.innerHTML = '<style>#cMsgs{border-top: 1px solid #000;margin-top: 10px;}</style><template><div class="msg"><label>Minute Value</label><input value="0" pattern="[0-9\,]{1,}" placeholder="0-59"><br><label>Message: </label><input class="m"><br><a>Delete</a></div></template><h3 class="descgen">These are sent on a regular schedule.</h3><span class="descdet">If minute value is set to 0, the message will be sent at 1:00, 2:00... if it is set to 0,30, it will be sent at 1:00, 1:30, 2:00....</span><span class="add">+</span><div id="cMsgs"></div>';
-
-biblio_cron_messages.bot.fixTemplates();
-
-biblio_cron_messages.container = document.getElementById('cMsgs');
-biblio_cron_messages.template = biblio_cron_messages.mainTabs.cronTab.querySelector('template');
-
-biblio_cron_messages.addNew = function() {
-    this.add({});
-};
-
-biblio_cron_messages.add = function(saveObj) {
-    var content = this.template.content;
-    content.querySelector('div').id = 'cron_m' + this.uMID;
-    content.querySelector('input').value = (typeof saveObj.minutes == 'string') ? saveObj.minutes : 0;
-    content.querySelector('.m').value = (typeof saveObj.message == 'string') ? saveObj.message : '';
-
-    this.container.appendChild(document.importNode(content, true));
-    this.container.querySelector('#cron_m' + this.uMID++ + '>a').addEventListener('click', this.bot.deleteMsg.bind(this), false); //Piggybacking, yay!
-};
-
-biblio_cron_messages.saveConfig = function() {
-    this.messages = [];
-    var ch = this.container.children;
-    for (var i=0; i<ch.length; i++) {
-        var a = ch[i].querySelector('input'), b = ch[i].querySelector('.m');
-        if (a.validity.valid && a.value.length > 0 && b.value.length > 0) {
-            this.messages.push({message: b.value, minutes: a.value});
-        }
+    function deleteMessage(event) {
+        ui.alert('Really delete this message?',
+        [
+            {text: 'Delete', style: 'danger', thisArg: event.target.parentElement, action: function() {
+                this.remove();
+                saveConfig();
+            }},
+            {text: 'Cancel'}
+        ]);
+        event.stopPropagation();
     }
 
-    localStorage.setItem('biblio_cron_messages_messages' + window.worldId, JSON.stringify(this.messages));
-};
-
-biblio_cron_messages.listener = function() {
-    var minute = new Date().getMinutes();
-    this.messages.forEach(function(msg) {
-        var msgMinutes = msg.minutes.split(',');
-        msgMinutes.forEach(function(min) {
-            if (min == minute) {
-                this.core.send(msg.message);
+    function saveConfig() {
+        var containers = Array.from(ex.tab.querySelector('#cMsgs').children);
+        ex.messages = [];
+        containers.forEach(function(container) {
+            var minutes = container.querySelector('input');
+            var message = container.querySelector('.m');
+            if (minutes.validity.valid && minutes.value.length && message.value.length) {
+                ex.messages.push({message: message.value, minutes: minutes.value});
             }
-        }.bind(this));
-    }.bind(this));
-    setTimeout(this.listener.bind(this), 60000);
-};
+        });
 
-biblio_cron_messages.mainTabs.cronTab.querySelector('.add').addEventListener('click', biblio_cron_messages.addNew.bind(biblio_cron_messages), false);
-biblio_cron_messages.mainTabs.cronTab.addEventListener('change', biblio_cron_messages.saveConfig.bind(biblio_cron_messages), false);
+        storage.set('biblio_cron_messages_messages', ex.messages);
+    }
+    ex.tab.querySelector('#cMsgs').addEventListener('change', saveConfig);
 
-biblio_cron_messages.messages.forEach(function(msg) {
-    this.add(msg);
-}.bind(biblio_cron_messages));
-biblio_cron_messages.saveConfig.call(biblio_cron_messages);
-biblio_cron_messages.listener.call(biblio_cron_messages);
+    ex.messages = storage.getObject('biblio_cron_messages_messages', []);
+    ex.messages.forEach(addMessage);
+    saveConfig();
+
+    function listener() {
+        if (!ex.running) {
+            return;
+        }
+        var minute = new Date().getMinutes();
+        ex.messages.forEach(function(msg) {
+            var msgMinutes = msg.minutes.replace(/ /g, '').split(',');
+            msgMinutes.forEach(function(min) {
+                if (min == minute) {
+                    ex.bot.send(msg.message);
+                }
+            });
+        });
+
+        setTimeout(listener, 60 * 1000);
+    }
+    listener();
+}(biblio_cron_messages, biblio_cron_messages.storage, biblio_cron_messages.ui));
